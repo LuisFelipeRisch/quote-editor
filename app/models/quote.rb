2a685bd372d583b2462ab391b9920271d1dab080
Luis Felipe Risch <luis.risch@gmail.com>
2023-12-28 21:50:54 -0300

    fix: turbo stream problems

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -5,7 +5,8 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
-  after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
-  after_update_commit -> { broadcast_replace_to "quotes" }
-  after_destroy_commit -> { broadcast_remove_to "quotes" }
+  broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
+  # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  # after_update_commit -> { broadcast_replace_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
 end
